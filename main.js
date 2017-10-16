b419d7ecfa23d4aa8e3647e94c87f4c107f7d754
gadjimagomedov <islam7581@mail.ru>
2017-10-16 21:16:00 +0300

    добавляем переменную , которая хранит в себе индекс элемента , для рачета позиции каждого слоя

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ let moveLayers = function(e){
             let layerStyle = layer.style,
                 divider = i / 100;
                 positionX = initialX * divider,
-                positionY = initialY * divider;
+                positionY = initialY * divider,
+                bottomPosition = (window.innerHeight / 2) * divider;
 
             
 
             layerStyle.transform = `translate3d(${positionX}px, ${positionY}px, 0)`;
-        });
-
-        
+            layerStyle.bottom = `-${bottomPosition}px`;
+        });       
 }
 
 window.addEventListener('mousemove', moveLayers );
